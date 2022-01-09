2733e6dc51881d40d5b9a372867f735e1378012d
npatelDev <91616869+npatelDev@users.noreply.github.com>
2022-01-08 22:51:14 -0800

    Add files via upload

---
+++ eclipse-workspacePersonal/AuthentProject/src/main/java/com/example/AuthentProject/AuthentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.AuthentProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentProjectApplication.class, args);
+	}
+
+}
---
+++ eclipse-workspacePersonal/AuthentProject/src/main/java/com/example/AuthentProject/Config/securityConfig.java
@@ -0,0 +1,46 @@
+package com.example.AuthentProject.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import com.example.AuthentProject.Services.MyUserDetailService;
+
+@Configuration
+@EnableWebSecurity
+public class securityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private MyUserDetailService userDetailService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailService);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+		.antMatchers("/login").permitAll().anyRequest().authenticated();
+	}
+	
+	
+	
+}
